aef6e84768a509788b8cc1e5b1981e52176d0bb3
PicturElements <namnpseudonym@gmail.com>
2019-09-01 02:53:53 +0200

    @qtxr/vue-form 1.0.2 - 2: Fixed exports

--- pkg/vue-form/index.js
+++ pkg/vue-form/index.js
@@ -1,1 +1,4 @@
-// TODO: write export code
+export { default as Checkbox } from "./src/checkbox.vue";
+export { default as Dropdown } from "./src/dropdown.vue";
+export { default as Input } from "./src/input.vue";
+export { default as Radio } from "./src/radio.vue";
