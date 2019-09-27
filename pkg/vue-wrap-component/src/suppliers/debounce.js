0121bb4db8fceca3a4bea01f33bc832f2cf3fc8d
PicturElements <namnpseudonym@gmail.com>
2019-09-27 18:16:00 +0700

    @qtxr/vue-wrap-component 2.0.2 - 6: Moved Debouncer to @qtxr/uc

--- pkg/vue-wrap-component/src/suppliers/debounce.js
+++ pkg/vue-wrap-component/src/suppliers/debounce.js
@@ -1,40 +1,22 @@
 export default {
-	use(wrapper, used, name, timeout = 50) {
-		const int = wrapper.internal;
-
-		int.debounce = int.debounce || {};
-		int.debounce[name] = new Debouncer(timeout);
-
-		wrapper.addData(name, int.debounce[name]);
-
-		if (used)
-			return;
-		
-		wrapper.addHook("beforeDestroy", _ => {
-			for (const k in int.debounce) {
-				if (int.debounce.hasOwnProperty(k))
-					int.debounce[k].clear();
-			}
-		});
+	init(Debouncer) {
+		return (wrapper, used, name, timeout = 50) => {
+			const int = wrapper.internal;
+	
+			int.debounce = int.debounce || {};
+			int.debounce[name] = new Debouncer(timeout);
+	
+			wrapper.addData(name, int.debounce[name]);
+	
+			if (used)
+				return;
+			
+			wrapper.addHook("beforeDestroy", _ => {
+				for (const k in int.debounce) {
+					if (int.debounce.hasOwnProperty(k))
+						int.debounce[k].clear();
+				}
+			});
+		};
 	}
 };
-
-class Debouncer extends Hookable {
-	constructor(timeout) {
-		super();
-		this.timeout = timeout;
-		this.timer = null;
-		this.callback = null;
-	}
-
-	debounce(callback, timeout) {
-		this.callback = callback;
-		this.clear();
-		const to = typeof timeout == "number" ? timeout : this.timeout;
-		this.timer = setTimeout(callback, to);
-	}
-
-	clear() {
-		clearTimeout(this.timer);
-	}
-}
