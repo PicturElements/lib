edada0ae537b8172bb30b913c6e1f1c0b400de2b
PicturElements <namnpseudonym@gmail.com>
2019-09-30 17:10:18 +0700

    @qtxr/utils 1.13.0 - 32: parseArgStr accepts regex argument separators

--- pkg/utils/src/parse-arg-str.js
+++ pkg/utils/src/parse-arg-str.js
@@ -4,15 +4,45 @@ export default function parseArgStr(str, argSeparator) {
 	return splitArgStr(str, argSeparator).map(parseStr);
 }
 
+const argSeparatorCache = {};
+
 function splitArgStr(str, argSeparator = ",") {
-	const args = [];
+	const args = [],
+		isRegexArgSep = argSeparator instanceof RegExp;
 	let quote = null,
 		structStackDepth = 0,
 		arg = "",
 		lastChar = null;
 
+	if (isRegexArgSep) {
+		const key = argSeparator.toString();
+
+		if (argSeparatorCache.hasOwnProperty(key))
+			argSeparator = argSeparatorCache[key];
+		else {
+			const flags = argSeparator.flags.indexOf("g") == -1 ?
+				`${argSeparator.flags}g` :
+				argSeparator.flags;
+
+			argSeparator = new RegExp(argSeparator.source, flags);
+			argSeparatorCache[key] = argSeparator;
+		}
+	}
+
 	for (let i = 0, l = str.length; i < l; i++) {
 		const char = str[i];
+
+		if (isRegexArgSep && !quote && !structStackDepth) {
+			argSeparator.lastIndex = i;
+			const ex = argSeparator.exec(str);
+
+			if (ex && ex.index == i) {
+				args.push(arg.trim());
+				arg = "";
+				i += ex[0].length - 1;
+				continue;
+			}
+		}
 
 		switch (char) {
 			case "\\":
