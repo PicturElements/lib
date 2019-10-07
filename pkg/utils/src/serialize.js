ecb0b45f850f967faff59125d57ec4c91263d406
PicturElements <namnpseudonym@gmail.com>
2019-10-07 08:29:02 +0700

    @qtxr/utils 1.16.0 - 35: Serialize function

---
+++ pkg/utils/src/serialize.js
@@ -0,0 +1,69 @@
+import { isObject } from "./is";
+import { repeat } from "./str";
+
+export default function serialize(fieldsOrItem, optionsOrIndentStr = {}) {
+	let options = optionsOrIndentStr;
+
+	if (typeof optionsOrIndentStr == "string") {
+		options = {
+			indentStr: optionsOrIndentStr
+		};
+	}
+
+	const indentStr = typeof options.indentStr == "string" ? options.indentStr : "\t",
+		startIndent = typeof options.indent == "number" ? options.indent || 0 : 0,
+		quoteChar = typeof options.quote == "string" ? options.quote : "\"";
+
+	const srz = (item, indent = 0) => {
+		switch (typeof item) {
+			case "string":
+				return `"${item}"`;
+			case "number":
+				return isNaN(item) || !isFinite(item) ? "null": String(item);
+			case "boolean":
+				return String(item);
+			case "object":
+				if (item == null)
+					return "null";
+				
+				if (Array.isArray(item)) {
+					const flat = !item.some(isObject);
+					if (flat)
+						return `[${item.map(srz).join(", ")}]`;
+
+					const out = [];
+
+					for (const elem of item) {
+						const serialized = srz(elem, indent + 1);
+
+						if (serialized !== undefined)
+							out.push(repeat(indentStr, indent + 1) + serialized);
+					}
+
+					return `[\n${out.join(",\n")}\n${repeat(indentStr, indent)}]`;
+				}
+				
+				if (isObject(item)) {
+					const out = [];
+
+					for (const k in item) {
+						const serialized = srz(item[k], indent + 1);
+
+						if (serialized !== undefined)
+							out.push(`${repeat(indentStr, indent + 1)}${quoteChar}${k}${quoteChar}: ${serialized}`);
+					}
+
+					if (!out.length)
+						return "{}";
+
+					return `{\n${out.join(",\n")}\n${repeat(indentStr, indent)}}`;
+				}
+
+				return "{}";
+			case "function":
+				return "{}";
+		}
+	};
+
+	return repeat(indentStr, startIndent) + srz(fieldsOrItem, startIndent);
+}
