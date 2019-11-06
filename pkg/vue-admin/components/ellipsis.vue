8fd25df8786d6d0d64f9e5fbbeb3bcd521407141
PicturElements <namnpseudonym@gmail.com>
2019-11-06 21:06:12 +0700

    @qtxr/vue-admin 1.4.2 - 44: Cleaned up for export

--- pkg/vue-admin/components/ellipsis.vue
+++ pkg/vue-admin/components/ellipsis.vue
@@ -3,7 +3,7 @@
 		.ellipsis-launcher(
 			ref="launcher"
 			@click="launchToggleMenu")
-			Icon.ico-ellipsis
+			slot(name="icon") ⋯
 		.ellipsis-menu(ref="menu")
 			template(v-for="option in options")
 				.ellipsis-menu-item(
@@ -12,12 +12,10 @@
 </template>
 
 <script>
-	import admin from "../admin";
-
 	import { requestFrame } from "@qtxr/utils";
-	import Icon from "./icon.vue";
+	import wc from "@qtxr/vue-wrap-component";
 
-	const component = admin.wrapC({
+	const component = wc.wrap({
 		name: "Ellipsis",
 		data: {
 			open: false,
@@ -100,108 +98,3 @@
 
 	export default component.export();
 </script>
-
-<style lang="scss">
-	@use "../assets/scss/theme.scss" as *;
-
-	$ellipsis-width: 16px;
-	$ellipsis-padding: 8px;
-	$ellipsis-border-width: 1px;
-
-	.ellipsis {
-		position: relative;
-		width: $ellipsis-width;
-		height: $ellipsis-width;
-		z-index: 10000;
-
-		.ellipsis-launcher {
-			position: relative;
-			width: $ellipsis-width;
-			height: $ellipsis-width;
-			padding: $ellipsis-padding;
-			margin: -$ellipsis-padding;
-			box-sizing: content-box;
-			cursor: pointer;
-			z-index: 10;
-
-			.adm-icon {
-				opacity: 0.5;
-			}
-
-			&:hover .adm-icon {
-				opacity: 1;
-			}
-		}
-
-		&.open {
-			transform: translateZ(10px);
-			z-index: 100000;
-
-			.ellipsis-launcher .adm-icon {
-				opacity: 1;
-			}
-		}
-
-		.ellipsis-menu {
-			visibility: hidden;
-			position: absolute;
-			top: 100%;
-			right: -($ellipsis-padding + $ellipsis-border-width);
-			padding: $ellipsis-padding;
-			margin-top: $ellipsis-padding - $ellipsis-border-width;
-			background: $card-background;
-			border: 1px solid $main-background;
-			border-radius: 2px 0 2px 2px;
-			min-width: 100px;
-
-			&:before {
-				content: "";
-				position: absolute;
-				top: -($ellipsis-width + 2 * $ellipsis-padding);
-				right: -$ellipsis-border-width;
-				width: $ellipsis-width;
-				height: $ellipsis-width;
-				box-sizing: content-box;
-				padding: $ellipsis-padding;
-				background: $card-background;
-				border: 1px solid $main-background;
-				border-bottom-width: 0;
-			}
-
-			.ellipsis-menu-item {
-				padding: 6px 10px;
-				white-space: nowrap;
-				cursor: pointer;
-
-				&:hover {
-					background: $main-background;
-				}
-			}
-		}
-
-		&:not(.open) .ellipsis-menu {
-			position: fixed;
-			top: 0;
-			left: 0;
-		}
-
-		&.open .ellipsis-menu {
-			visibility: visible;
-		}
-
-		&.flipped {
-			.ellipsis-menu {
-				top: auto;
-				bottom: 100%;
-				margin: 0 0 ($ellipsis-padding - $ellipsis-border-width) 0;
-
-				&:before {
-					border-top-width: 0;
-					border-bottom-width: $ellipsis-border-width;
-					top: auto;
-					bottom: -($ellipsis-width + 2 * $ellipsis-padding);
-				}
-			}
-		}
-	}
-</style>
