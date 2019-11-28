f5a11ce33ca7a504cf7655325d837ab28cbf597d
PicturElements <namnpseudonym@gmail.com>
2019-11-28 02:27:54 +0100

    @qtxr/vue-form 2.4.3 - 86: Updated style, initial support for formatted input, updated dependencies

---
+++ pkg/vue-form/src/inputs/formatted.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+	.input-wrapper.formatted.inp-formatted
+		.formatted-content-editable(
+			v-once
+			contentEditable="true"
+			@click="updateSelection"
+			@focus="updateSelection"
+			ref="content")
+			| test, 
+			span in span, 
+			span
+				| in span again, 
+				span span in span
+</template>
+
+<script>
+	import Form, { Formatted } from "@qtxr/form";
+
+	export default {
+		name: "Formatted",
+		methods: {
+			trigger(count) {
+				Form.trigger(this.input, null);
+			},
+			check(evt) {
+				Form.check(this.input, evt, evt.target.value);
+			},
+			updateSelection() {
+				this.input.updateSelectionData(this.$refs.content);
+			},
+			res(val) {
+				if (typeof val == "function")
+					return val.call(this, this.input);
+
+				return val;
+			},
+			isMobile() {
+				const mobileQuery = this.mobileQuery || this.meta.mobileQuery || "(max-aspect-ratio: 1/1) and (max-width: 700px)";
+				return matchMedia(mobileQuery).matches;
+			}
+		},
+		props: {
+			input: Formatted,
+			mobileQuery: String,
+			meta: {
+				type: Object,
+				default: _ => ({})
+			}
+		}
+	}
+</script>
