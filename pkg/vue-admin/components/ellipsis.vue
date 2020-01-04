8ca71f63c78c1053c9ef9496cf3230353bf4190b
PicturElements <namnpseudonym@gmail.com>
2020-01-05 00:56:56 +0100

    @qtxr/vue-admin 1.9.9 - 113: Improved Ellipsis component, updated dependencies

--- pkg/vue-admin/components/ellipsis.vue
+++ pkg/vue-admin/components/ellipsis.vue
@@ -1,25 +1,35 @@
 <template lang="pug">
-	.ellipsis(:class="{ open, flipped }")
+	.ellipsis(:class="[ open ? 'open' : null, menuDirection ]")
 		.ellipsis-launcher(
 			ref="launcher"
 			@click="launchToggleMenu")
 			slot(name="icon") ⋯
-		.ellipsis-menu(ref="menu")
+		.ellipsis-menu(
+			:style="menuStyle"
+			ref="menu")
+			.ellipsis-menu-nub
+				slot(name="icon") ⋯
 			template(v-for="option in options")
 				.ellipsis-menu-item(
 					v-if="display(option)"
-					@click="dispatchAction(option)") {{ res(option.title, option) }}
+					:class="{ disabled: disabled(option) }"
+					@click="evt => dispatchAction(evt, option)") {{ res(option.title, option) }}
 </template>
 
 <script>
 	import { requestFrame } from "@qtxr/utils";
 	import wc from "@qtxr/vue-wrap-component";
 
+	const PADDING = 30,
+		BOTTOM_BIAS = 0.5;
+
 	const component = wc.wrap({
 		name: "Ellipsis",
 		data: {
 			open: false,
-			flipped: false
+			menuStyle: null,
+			menuDirection: false,
+			updateLoopInitialized: false
 		},
 		methods: {
 			launchToggleMenu() {
@@ -33,21 +43,43 @@
 				});
 			},
 			openMenu() {
-				const mbcr = this.$refs.menu.getBoundingClientRect(),
-					viewport = this.getViewportElem();
-				
-				if (viewport) {
-					const lbcr = this.$refs.launcher.getBoundingClientRect(),
-						vbcr = viewport.getBoundingClientRect(),
-						menuBottom = lbcr.bottom + mbcr.height;
-
-					this.flipped = menuBottom > vbcr.bottom || menuBottom > window.innerHeight;
-				}
-
 				this.open = true;
+				this.initUpdateLoop();
 			},
 			closeMenu() {
 				this.open = false;
+			},
+			initUpdateLoop() {
+				if (!this.updateLoopInitialized) {
+					this.updateLoopInitialized = true;
+					this.updateMenu();
+				}
+			},
+			updateMenu() {
+				if (!this.open || !this.$refs.menu) {
+					this.dropdownStyle = null;
+					this.updateLoopInitialized = false;
+					return;
+				}
+
+				const lbcr = this.$refs.launcher.getBoundingClientRect(),
+					mbcr = this.$refs.menu.getBoundingClientRect(),
+					bottomAvailable = window.innerHeight - lbcr.bottom - PADDING,
+					topAvailable = lbcr.top - PADDING,
+					placeBottom = bottomAvailable > mbcr.height,
+					maxHeight = placeBottom ? bottomAvailable : topAvailable;
+
+				this.menuStyle = {
+					position: "fixed",
+					top: placeBottom ? `${lbcr.top + lbcr.height}px` : null,
+					bottom: placeBottom ? null : `${window.innerHeight - lbcr.top}px`,
+					right: `${window.innerWidth - lbcr.right - 1}px`,
+					maxHeight: `${maxHeight}px`
+				};
+
+				this.menuDirection = placeBottom ? "place-bottom" : "place-top";
+
+				requestFrame(_ => this.updateMenu());
 			},
 			display(option) {
 				if (typeof option.display == "function")
@@ -55,8 +87,16 @@
 
 				return option.hasOwnProperty("display") ? Boolean(option.display) : true;
 			},
-			dispatchAction(option) {
-				if (typeof option.action == "function")
+			disabled(option) {
+				if (typeof option.disabled == "function")
+					return Boolean(option.disabled(option, this.data));
+
+				return option.hasOwnProperty("disabled") ? Boolean(option.disabled) : true;
+			},
+			dispatchAction(evt, option) {
+				if (this.disabled(option))
+					evt.stopPropagation();
+				else if (typeof option.action == "function")
 					option.action(option, this.data);
 			},
 			res(val, option) {
@@ -64,21 +104,6 @@
 					return val.call(this, option, this.data);
 
 				return val;
-			},
-			getViewportElem() {
-				if (!this.viewport)
-					return null;
-				
-				if (typeof this.viewport == "string")
-					return document.querySelector(this.viewport);
-				
-				if (this.viewport instanceof HTMLElement)
-					return this.viewport;
-
-				if (this.viewport.$el instanceof HTMLElement)
-					return this.viewport.$el;
-
-				return null;
 			}
 		},
 		computed: {},
