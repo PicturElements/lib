83ca64155fcba36b498f91fa3fbc9bc6065997f2
PicturElements <namnpseudonym@gmail.com>
2020-01-06 04:19:40 +0100

    @qtxr/vue-admin 1.10.1 - 116: Fixed TimedButton export

--- pkg/vue-admin/components/index.js
+++ pkg/vue-admin/components/index.js
@@ -4,5 +4,6 @@ export { default as Ellipsis } from "./ellipsis.vue";
 export { default as ListingBox } from "./listing-box.vue";
 export { default as Listing } from "./listing.vue";
 export { default as LoadingBox } from "./loading-box.vue";
+export { default as TimedButton } from "./timed-button.vue";
 export { default as UtilBox } from "./util-box.vue";
 export { default as Viz } from "./viz.vue";
