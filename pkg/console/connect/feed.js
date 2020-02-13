38ef7350e82c3fb72bf4d825b8cded9eab533e92
PicturElements <namnpseudonym@gmail.com>
2020-02-13 13:24:49 +0100

    @qtxr/console 1.0.1 - 1: Updated dependencies, feed

--- pkg/console/connect/feed.js
+++ pkg/console/connect/feed.js
@@ -8,5 +8,13 @@ feed.add("c", activeFeed => {
 	const c = new Console({
 		collapsed: true
 	});
+
+	const logTypes = ["log", "info", "warn", "error"];
+
+	setInterval(_ => {
+		const type = logTypes[~~(Math.random() * 4)];
+		console[type](type);
+	}, 1000);
+
 	return c;
 });
