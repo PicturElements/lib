01000338b63bee4ae21df6f8c5c2138c55ddac58
PicturElements <namnpseudonym@gmail.com>
2020-10-17 19:36:43 +0200

    @qtxr/utils 1.71.1 - 160: Fixed recursive resolving of nested preset references

--- pkg/utils/src/presets.js
+++ pkg/utils/src/presets.js
@@ -4,7 +4,6 @@ import {
 } from "./internal/merge-obj";
 import { isObject } from "./is";
 import inject from "./inject";
-import serialize from "./serialize";
 import hasOwn from "./has-own";
 import clone from "./clone";
 
@@ -22,20 +21,21 @@ function addPreset(presets, key, preset) {
 }
 
 function mergePresets(data, presets, config = {}) {
-	const visitedKeys = {};
+	const visitedPresets = {};
 
-	const merge = out => {
-		const mrg = (outData, preset, key, force) => {
-			if (!key || !preset || (!force && hasOwn(visitedKeys, key)))
+	const merge = (out, root) => {
+		const mrg = (outData, preset, force) => {
+			if (!preset || (!force && typeof preset == "string" && hasOwn(visitedPresets, preset)))
 				return null;
 
-			visitedKeys[key] = true;
+			if (typeof preset == "string")
+				visitedPresets[preset] = true;
 
 			if (Array.isArray(preset)) {
 				// Run in reverse since inject calls don't overwrite written fields,
 				// thereby giving later presets precedence over earlier presets
 				for (let i = preset.length - 1; i >= 0; i--)
-					outData = mrg(outData, preset[i], key, true) || outData;
+					outData = mrg(outData, preset[i], true) || outData;
 
 				return outData;
 			}
@@ -43,18 +43,21 @@ function mergePresets(data, presets, config = {}) {
 			if (isObject(preset)) {
 				return inject(
 					outData,
-					merge(preset),
+					merge(
+						clone(preset, config),
+						false
+					),
 					config.injectConfig
 				);
 			}
 
 			if (typeof preset != "string") {
-				console.error("Cannot inject preset: config must be a string name or object");
+				console.error("Cannot inject preset: config must be a reference string or object");
 				return null;
 			}
 
 			if (!hasOwn(presets, preset)) {
-				console.error(`Cannot inject preset: ${serialize(preset)} is not a valid name`);
+				console.error(`Cannot inject preset: ${preset} is not a known preset`);
 				return null;
 			}
 
@@ -67,22 +70,34 @@ function mergePresets(data, presets, config = {}) {
 
 		const k = config.key || config.keys,
 			keys = Array.isArray(k) ? k : [k];
+		let changed = true;
 
-		if (hasOwn(config, "defaultKey"))
-			out = mrg(out, presets[config.defaultKey], config.defaultKey) || out;
+		while (changed) {
+			changed = false;
 
-		for (let i = 0, l = keys.length; i < l; i++) {
-			const key = keys[i],
-				preset = out[key];
+			for (let i = 0, l = keys.length; i < l; i++) {
+				const key = keys[i],
+					preset = out[key];
 
-			delete out[key];
-			out = mrg(out, preset, key) || out;
+				if (!hasOwn(out, key))
+					continue;
+	
+				changed = true;
+				delete out[key];
+				out = mrg(out, preset, false) || out;
+			}
 		}
+
+		if (root && hasOwn(config, "defaultKey"))
+			out = mrg(out, presets[config.defaultKey], false) || out;
 
 		return out;
 	};
 
-	return merge(clone(data, config));
+	return merge(
+		clone(data, config),
+		true
+	);
 }
 
 export {
