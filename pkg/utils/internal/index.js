6fbbfebea48801d604604fa2d561d7614656a372
PicturElements <namnpseudonym@gmail.com>
2020-11-16 04:00:49 +0100

    @qtxr/utils 1.75.2 - 168: Exposed constants and lookups

--- pkg/utils/internal/index.js
+++ pkg/utils/internal/index.js
@@ -2,4 +2,8 @@ export * from "../src/internal/cache";
 export * from "../src/internal/merge-obj";
 export * from "../src/internal/poly";
 
+// Export data previously only internally available for back compat
+export * from "../src/data/constants";
+export * from "../src/data/lookups";
+
 export { default as VolatileMap } from "../src/internal/volatile-map";
